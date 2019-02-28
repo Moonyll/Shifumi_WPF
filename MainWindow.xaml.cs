0700dc42bba59fbe98fb530972dc8f4c9338f3bb
Moonyll <s.pouwel@laposte.net>
2019-02-28 15:57:43 +0100

    Shifumi_WPF

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+
+namespace Shifumi
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Déclaration des variables
+        int index;
+        string[] shifumi = new string[] { "Pierre", "Feuille", "Ciseaux" }; // Tableau Pierre Feuille Ciseaux
+        int wins, loss, deuce, games = 0; // Victoires, égalités, jeux
+        public MainWindow()
+        {
+        InitializeComponent();
+
+        }
+        private void Choice(object sender, RoutedEventArgs e)
+        {
+        Button button_choice = (Button)sender;
+
+        //int index = System.Windows.Controls.Grid.GetColumn(button_choice); // Récupère l'index de la colonne du button choisi
+        string choix = $"{button_choice.Content}";
+        index = Array.IndexOf(shifumi, choix);
+
+        joueur.Text= $"{"Le Joueur a choisi : "}{choix} {index}";
+        
+        }
+        private void Play(object sender, RoutedEventArgs e)
+        {
+        Random random_number = new Random();
+        int random_choice = random_number.Next(0, 3);
+        var combine = index.ToString() + random_choice.ToString();
+        int new_index = index;
+
+            Button button_play = (Button)sender;
+
+        ordinateur.Text = $"{"L'ordinateur a choisi : "}{shifumi[random_choice]}";
+            if (combine == "02" || combine == "10" || combine == "21")
+            {
+            wins++;
+            games++;
+            resultat.Text = $"{shifumi[new_index]}{" Versus "}{shifumi[random_choice]}{" Joueur Gagne "}{" Victoires : "}{wins}{ " Défaites : "}{loss}{ " Egalités : "}{deuce}";
+            }
+            else if (combine == "20" || combine == "01" || combine == "12")
+            {
+            loss++;
+            games++;
+            resultat.Text = $"{shifumi[new_index]}{" Versus "}{shifumi[random_choice]}{" Joueur Perd "}{"Victoires : "}{wins}{ " Défaites : "}{loss}{ " Egalités : "}{deuce}";
+            }
+            else
+            {
+            deuce++;
+            games++;
+            resultat.Text = $"{shifumi[new_index]}{" Versus "}{shifumi[random_choice]}{" Egalité "}{" Victoires : "}{wins}{ " Défaites : "}{loss}{ " Egalités : "}{deuce}";
+            }
+        }
+    }
+}
